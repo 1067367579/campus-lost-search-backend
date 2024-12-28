0f1d2e3d0017f651fb6ffb86227cca708ea53875
lujingxiang <1067367579@qq.com>
2024-12-28 13:00:54 +0800

    构建后端框架

---
+++ src/main/java/com/example/campuslostsearch/CampusLostSearchApplication.java
@@ -0,0 +1,13 @@
+package com.example.campuslostsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusLostSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusLostSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/campuslostsearch/CampusLostSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campuslostsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusLostSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
